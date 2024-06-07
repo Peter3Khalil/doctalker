1e96fd96665763bbbdad278656b53179100d98e6
Peter Khalil <peter.khalil.fahmy@gmail.com>
2024-06-07 19:00:20 +0300

    :lock: Adding Some checks to axios interceptors

--- src/features/client.ts
+++ src/features/client.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { AxiosError, isAxiosError } from 'axios';
 const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
@@ -7,4 +7,24 @@ instance.interceptors.request.use((config) => {
 
   return config;
 });
+
+instance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (isAxiosError(error)) {
+      const axiosError = error as AxiosError;
+      const { response } = axiosError;
+
+      if (response?.status === 401 || response?.status === 403) {
+        if (typeof window !== 'undefined') {
+          window.location.href = '/login';
+        }
+      }
+    }
+
+    return Promise.reject(error);
+  },
+);
 export default instance;
