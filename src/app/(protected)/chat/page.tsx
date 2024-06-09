7d8cd4407300fb81b74fcc075c025ce8c39aed27
Peter Khalil <peter.khalil.fahmy@gmail.com>
2024-06-09 20:30:58 +0300

    :recycle: Refactoring Chat Page

--- src/app/(protected)/chat/page.tsx
+++ src/app/(protected)/chat/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import FileUploader from '@/components/FileUploader';
 import FolderUploader from '@/components/premiumComponents/FolderUploader';
+import { ScrollArea } from '@/components/ui/scroll-area';
 import { useUser } from '@/providers/user-provider';
 import React from 'react';
 
@@ -8,18 +9,20 @@ const Chat: React.FC = () => {
   const { user } = useUser();
 
   return (
-    <div className="flex h-full w-full flex-col items-center gap-3 overflow-auto px-10 pb-6 pt-12 md:gap-16">
-      <article className="prose prose-lg text-center dark:prose-invert *:my-2">
-        <h1 className="gradient bg-clip-text dark:to-foreground">
-          Hello, {user?.firstName}
-        </h1>
-        <p>How can we help you today?</p>
-      </article>
-      <div className="flex w-full flex-col items-center justify-center gap-4 md:max-w-3xl md:flex-row md:items-baseline">
-        <FileUploader />
-        <FolderUploader />
+    <ScrollArea className="h-full w-full">
+      <div className="flex flex-col items-center gap-3 px-10 pb-6 pt-12 md:gap-8">
+        <article className="prose prose-lg text-center dark:prose-invert *:my-2">
+          <h1 className="gradient bg-clip-text dark:to-foreground">
+            Hello, {user?.firstName}
+          </h1>
+          <p>How can we help you today?</p>
+        </article>
+        <div className="flex w-full flex-col items-center justify-center gap-4 md:max-w-3xl md:flex-row md:items-baseline">
+          <FileUploader />
+          <FolderUploader />
+        </div>
       </div>
-    </div>
+    </ScrollArea>
   );
 };
 
